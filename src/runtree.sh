9d4a1e5af750d93ea18f1c0b30ae1ca6d1ccd18b
JinsuRhee <jinsu.rhee@gmail.com>
2020-11-04 14:07:14 +0900

    minor

---
+++ src/runtree.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+sed -i "41c\ \ P_VRrun_snap = [030, 090, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+sed -i "41c\ \ P_VRrun_snap = [100, 150, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+sed -i "41c\ \ P_VRrun_snap = [151, 200, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+sed -i "41c\ \ P_VRrun_snap = [201, 240, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+sed -i "41c\ \ P_VRrun_snap = [241, 280, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+sed -i "41c\ \ P_VRrun_snap = [281, 320, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+sed -i "41c\ \ P_VRrun_snap = [325, 335, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+sed -i "41c\ \ P_VRrun_snap = [340, 350, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+sed -i "41c\ \ P_VRrun_snap = [355, 365, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+sed -i "41c\ \ P_VRrun_snap = [370, 380, 1]" ~/Work_Fornax/settings.nml && idl -e main
+
+
+
+
+#sed -i "41c\ \ P_VRrun_snap = [385, 395, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+#sed -i "41c\ \ P_VRrun_snap = [400, 410, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+#sed -i "41c\ \ P_VRrun_snap = [415, 425, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+#sed -i "41c\ \ P_VRrun_snap = [430, 441, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+#sed -i "41c\ \ P_VRrun_snap = [445, 455, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+#sed -i "41c\ \ P_VRrun_snap = [461, 475, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+#sed -i "41c\ \ P_VRrun_snap = [480, 490, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+#sed -i "41c\ \ P_VRrun_snap = [495, 506, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+#sed -i "41c\ \ P_VRrun_snap = [510, 520, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+#sed -i "41c\ \ P_VRrun_snap = [531, 545, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+#sed -i "41c\ \ P_VRrun_snap = [550, 561, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+#sed -i "41c\ \ P_VRrun_snap = [571, 580, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+#sed -i "41c\ \ P_VRrun_snap = [585, 600, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+#sed -i "41c\ \ P_VRrun_snap = [605, 615, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+#sed -i "41c\ \ P_VRrun_snap = [620, 640, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+#sed -i "41c\ \ P_VRrun_snap = [645, 655, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+#sed -i "41c\ \ P_VRrun_snap = [660, 671, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+#sed -i "41c\ \ P_VRrun_snap = [675, 685, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+#sed -i "41c\ \ P_VRrun_snap = [690, 701, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+#sed -i "41c\ \ P_VRrun_snap = [705, 720, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+#sed -i "41c\ \ P_VRrun_snap = [730, 740, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+#sed -i "41c\ \ P_VRrun_snap = [745, 755, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+#sed -i "41c\ \ P_VRrun_snap = [760, 775, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+#sed -i "41c\ \ P_VRrun_snap = [779, 820, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+#sed -i "41c\ \ P_VRrun_snap = [825, 835, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+#sed -i "41c\ \ P_VRrun_snap = [840, 870, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+#sed -i "41c\ \ P_VRrun_snap = [880, 890, 1]" ~/Work_Fornax/settings.nml && idl -e main &
+#sed -i "41c\ \ P_VRrun_snap = [900, 900, 1]" ~/Work_Fornax/settings.nml && idl -e main
